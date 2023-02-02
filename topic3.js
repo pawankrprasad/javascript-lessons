717a08752f39c5ab5d43ff017e8e7639b7160ddb
Pawan Kumar Prasad <pkprasad06@gmail.com>
2023-02-02 19:38:17 +0530

    day1

---
+++ topic3.js
@@ -0,0 +1,38 @@
+
+//Template literal
+/**
+ *  Back-Tics `` 
+    Quotes Inside Strings
+    Multiline Strings
+    Interpolation ${}
+ */
+const number1 = 50;
+const number2 = 60;
+
+ //console.log("Sum of "+ number1 + " and "+ number2+ " is "+ result)
+
+ console.log(`Sum of ${number1} and ${number2} is ${number1+number2}`)
+
+
+ //const message = 'This is my first\n \'hello\' message';
+
+
+//  const message = 
+//  `This is my first
+//   'hello' message`
+
+// console.log(message)
+
+
+// This is my first
+// 'hello' message
+
+// const message = 'This is my first\n \'hello\' message';
+
+// console.log(message)
+
+// const num1=30;
+// const num2=40;
+// const result = `The result of the ‘addition’ is : ${num1+num2}`
+
+// console.log(result)
